3dc88bac82f4149d7313e38314e8b2f335f60fee
Karabo <karabo.maragelo@gmail.com>
2021-03-24 11:51:02 +0200

    first

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,7 +10,7 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello There mama");
+            Console.WriteLine("Hello There mamas");
             Console.ReadLine();
         }
     }
